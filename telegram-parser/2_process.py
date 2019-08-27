f080068d41a4a45eef7bda686a8ea57ddc2ed1bc
gebeto <slavik.nychkalo@gmail.com>
2019-08-27 21:53:13 +0300

    add js solution parsing

--- telegram-parser/2_process.py
+++ telegram-parser/2_process.py
@@ -1,11 +1,16 @@
 import selects
+import jsbin
 
 
-posts = selects.select_all_posts.cache()
-jsbin_messages = selects.select_all_jsbin_messages.cache()
 
-for post in posts:
-	pass
+solution = jsbin.get_solution("https://jsbin.com/lixazat/edit?js,console")
+print(solution)
 
-posts
-jsbin_messages
+# posts = selects.select_all_posts.cache()
+# jsbin_messages = selects.select_all_jsbin_messages.cache()
+
+# for post in posts:
+# 	pass
+
+
+
--- telegram-parser/jsbin.py
+++ telegram-parser/jsbin.py
@@ -1,10 +1,31 @@
 import re
+import requests
 
 
 url_regex = re.compile(r"https:\/\/jsbin\.com\/[\w\W]+?\/")
 
 
-def parse_jsbin(jsbin):
-	headers = { "referer": "https://jsbin.com/cusunitila/edit" }
+def parse_jsbin(url):
+	headers = { "referer": url }
 	response = requests.get('https://jsbin.com/bin/start.js', headers=headers)
-	return response.content
+	return response.text
+
+
+null = None
+false = False
+true = True
+document = None
+this = None
+
+def start(obj, *args, **kwargs):
+	return obj
+
+
+def get_solution(url):
+	res = parse_jsbin(url)
+	res = res.strip()
+	res = "".join(res.rsplit(";", 1))
+
+	res = eval(res)
+
+	return res["javascript"]
